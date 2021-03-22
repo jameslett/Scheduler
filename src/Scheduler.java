4613bfeed1060a8f3fd816c1a563e3312fb4df3b
jameslett <jameslett1990@gmail.com>
2021-03-22 09:42:03 -0400

    first

---
+++ src/Scheduler.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Scheduler extends Application {
+
+
+    public static void main(String[] args) {
+
+        launch(args);
+
+
+    }
+
+
+    @Override
+    public void start(Stage primaryStage){
+
+    }
+
+
+
+}
